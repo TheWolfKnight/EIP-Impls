955173022612e182ca85da9ba11eed5e14061f33
Deblight <116873921+Deblight@users.noreply.github.com>
2025-10-02 12:29:47 +0200

    Add files via upload

---
+++ ProducerContentRouting/ConsumerContentRouting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+};
+
+var conn = await factory.CreateConnectionAsync();
+await using var ch = await conn.CreateChannelAsync();
+
+
+await ch.ExchangeDeclareAsync(exchange: "headersexchange", type: ExchangeType.Headers, durable: false, autoDelete: true);
+await ch.QueueDeclareAsync(queue: "letterbox", durable: false, exclusive: false, autoDelete: false);
+
+var bindingArgs = new Dictionary<string, object> {
+    { "x-match", "all" }, 
+    { "name", "Erik" },
+    { "age",  "28" }
+};
+
+await ch.QueueBindAsync(queue: "letterbox", exchange: "headersexchange", routingKey: "", arguments: bindingArgs);
+
+var consumer = new AsyncEventingBasicConsumer(ch);
+consumer.ReceivedAsync += async (_, ea) =>
+{
+    var msg = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"Received new message: {msg}");
+    await ch.BasicAckAsync(ea.DeliveryTag, multiple: false);
+};
+
+await ch.BasicConsumeAsync(queue: "letterbox", autoAck: false, consumer: consumer);
+
+Console.WriteLine("Consuming (press Ctrl+C to exit)");
+await Task.Delay(-1);
---
+++ ProducerContentRouting/ProducerContentRouting/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+   
+};
+
+var conn = await factory.CreateConnectionAsync();
+await using var ch = await conn.CreateChannelAsync();
+
+await ch.ExchangeDeclareAsync(exchange: "headersexchange", type: ExchangeType.Headers, durable: false, autoDelete: true);
+
+var message = "This message will be sent with headers";
+var body = Encoding.UTF8.GetBytes(message);
+
+var props = new BasicProperties
+{
+    ContentType = "text/plain",
+    Headers = new Dictionary<string, object> {
+        { "name", "Erik" },
+        { "age",  "28" } 
+    }
+};
+
+await ch.BasicPublishAsync(exchange: "headersexchange", routingKey: "", mandatory: false, body: body, basicProperties: props);
+
+Console.WriteLine($"Sent message: {message}");
